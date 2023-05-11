33cad38562f199b7c2c9a6ec0adc404d232eac4f
ikramdhib <dhibikram50@gmail.com>
2023-05-11 22:51:11 +0100

    "updates"

--- src/com/mycompany/myapp/utils/MaintenanceStatics.java
+++ src/com/mycompany/myapp/utils/MaintenanceStatics.java
@@ -15,6 +15,6 @@ public class MaintenanceStatics {
     public static String DISPLAY_ALL_MAINTENANCE="http://127.0.0.1:8000/maintenancemobile";
     public static String ADD_MAINTENANCE="http://127.0.0.1:8000/addmaintenancemovile";
     public static String UPDATE_MAINTENANCE="http://127.0.0.1:8000/updateMaterielmobile/";
-    public static String GET_ONE="http://127.0.0.1:8000/getOnemobile/";
+    public static String GET_ONE="http://127.0.0.1:8000/getOneMaint";
     
 }
