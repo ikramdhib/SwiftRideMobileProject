1f9ee991321027c7b0923d3ca6bc503b6ace857d
ikramdhib <dhibikram50@gmail.com>
2023-06-20 13:19:19 +0100

    "last push"

--- src/com/mycompany/myapp/utils/MaintenanceStatics.java
+++ src/com/mycompany/myapp/utils/MaintenanceStatics.java
@@ -14,7 +14,7 @@ public class MaintenanceStatics {
     public static String DELETE_MAINTENANCE="http://127.0.0.1:8000/deleteMaintenencemobile";
     public static String DISPLAY_ALL_MAINTENANCE="http://127.0.0.1:8000/maintenancemobile";
     public static String ADD_MAINTENANCE="http://127.0.0.1:8000/addmaintenancemovile";
-    public static String UPDATE_MAINTENANCE="http://127.0.0.1:8000/updateMaterielmobile/";
+    public static String UPDATE_MAINTENANCE="http://127.0.0.1:8000/updateMaintenanceMobile";
     public static String GET_ONE="http://127.0.0.1:8000/getOneMaint";
     
 }
